aff0d7f07bc45429fa9a0d51fa096a5a91512e4a
Almeerah <almeerahsaleem@gmail.com>
2024-09-03 11:43:15 +0500

    ui edit

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,34 +1,76 @@
-import { useSocketContext } from "../../context/SocketContext";
-import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import Conversation from "./Conversation";
 
-const Conversation = ({ conversation, lastIdx }) => {
-  const { selectedConversation, setSelectedConversation } = useConversation();
-  const { onlineUsers } = useSocketContext();
-
-  const isSelected = selectedConversation?._id === conversation._id;
-  const isOnline = onlineUsers.includes(conversation._id);
+const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+  console.log("CONVERSATIONS : ", conversations);
   return (
-    <>
-      <div
-        className={`flex gap-2 items-center  hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
-        ${isSelected ? "bg-sky-500" : ""}`}
-        onClick={() => setSelectedConversation(conversation)}
-      >
-        <div className={`avatar ${isOnline ? "online" : ""}`}>
-          <div className="w-12 rounded-full">
-            <img src={conversation.profile} alt="user avatar" />
-          </div>
-        </div>
-        <div className="flex flex-col flex-1">
-          <div className="flax gap-3 justify-between">
-            <p className="font-bold">{conversation.username}</p>
-          </div>
-        </div>
-      </div>
+    <div className="py-2 flex flex-col overflow-y-auto max-h-[500px]">
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          lastIdx={idx === conversations.length - 1} //index of last conv just to be sure if it's the last
+        />
+      ))}
 
-      {!lastIdx && <div className="divider my-0 py-1 h-1" />}
-    </>
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
+    </div>
   );
 };
 
-export default Conversation;
+export default Conversations;
+
+//import Conversation from "./Conversation";
+
+// const Conversations = () => {
+//   return (
+//     <div className="py-2 flex flex-col overflow-auto">
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//     </div>
+//   );
+// };
+
+// export default Conversations;
+
+// import { useSocketContext } from "../../context/SocketContext";
+// import useConversation from "../../zustand/useConversation";
+
+// const Conversation = ({ conversation, lastIdx }) => {
+//   const { selectedConversation, setSelectedConversation } = useConversation();
+//   const { onlineUsers } = useSocketContext();
+
+//   const isSelected = selectedConversation?._id === conversation._id;
+//   const isOnline = onlineUsers.includes(conversation._id);
+//   return (
+//     <>
+//       <div
+//         className={`flex gap-2 items-center  hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+//         ${isSelected ? "bg-sky-500" : ""}`}
+//         onClick={() => setSelectedConversation(conversation)}
+//       >
+//         <div className={`avatar ${isOnline ? "online" : ""}`}>
+//           <div className="w-12 rounded-full">
+//             <img src={conversation.profile} alt="user avatar" />
+//           </div>
+//         </div>
+//         <div className="flex flex-col flex-1">
+//           <div className="flax gap-3 justify-between">
+//             <p className="font-bold">{conversation.username}</p>
+//           </div>
+//         </div>
+//       </div>
+
+//       {!lastIdx && <div className="divider my-0 py-1 h-1" />}
+//     </>
+//   );
+// };
+
+// export default Conversation;
